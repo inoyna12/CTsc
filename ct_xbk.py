9a35e44a6b0eb59c7b9c360fcaca91639dc3a195
inoyna12 <inoyna12@163.com>
2022-10-21 00:25:43 +0800

    auto push 2022年10月21日 00:25:38

--- ct_xbk.py
+++ ct_xbk.py
@@ -33,7 +33,10 @@ params = {
 }
 response = requests.post(url=url, params=params, headers=headers).json()
 rep = response['giod']
-send('线报酷签到', rep)
 print("当前积分:", rep)
+title = "GlaDOS签到通知"
+content = "GlaDOS签到通知"
+send(title,content)
+
 
 # print(response.text)
