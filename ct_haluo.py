eeddcb9f1cff35737d1699b8493c4a162183d90c
inoyna12 <inoyna12@163.com>
2022-10-23 15:57:37 +0800

    auto push 2022年10月23日 15:57:31

--- ct_haluo.py
+++ ct_haluo.py
@@ -1,21 +1,8 @@
-'''
-
-cron: 6 14 * * * ct_haluo.py
-new Env('哈啰签到');
-
-'''
 import requests
-#import os
-
-# from sendNotify import send
-# from os import environ
-
-# cookie = os.environ["xbkcookie"]
-url = "https://api.hellobike.com/api"
 
 headers = {
     'Host': 'api.hellobike.com',
-    'content-length': '147',
+    # 'content-length': '147',
     'accept': 'application/json, text/plain, */*',
     'origin': 'https://m.hellobike.com',
     'sec-fetch-dest': 'empty',
@@ -26,7 +13,7 @@ headers = {
     'sec-fetch-site': 'same-site',
     'sec-fetch-mode': 'cors',
     'referer': 'https://m.hellobike.com/AppPlatformH5/latest/pr_index_bounty.html',
-    'accept-encoding': 'gzip, deflate',
+    # 'accept-encoding': 'gzip, deflate',
     'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
 }
 
@@ -36,10 +23,5 @@ params = {
 
 data = '{from:h5,systemCode:62,platform:4,version:6.25.0,action:common.welfare.signAndRecommend,token:cfb4950b8c4141bcba6c0c2a0144f0af}'
 
-response = requests.post(url=url, params=params, headers=headers, data=data)
+response = requests.post('https://api.hellobike.com/api', params=params, headers=headers, data=data)
 print(response.text)
-#rep = response['data']['bountyCountToday']
-#print("获得奖励金:", rep)
-#title = "哈啰签到"
-#content = "获得奖励金: " + rep
-#send(title,content)
