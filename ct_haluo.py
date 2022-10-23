973244aafea553deeca369d97a85a52e1be7dcdc
inoyna12 <inoyna12@163.com>
2022-10-23 16:49:43 +0800

    auto push 2022年10月23日 16:49:38

--- ct_haluo.py
+++ ct_haluo.py
@@ -1,4 +1,19 @@
+'''
+
+cron: 6 14 * * * ct_haluo.py
+new Env('哈啰签到');
+
+'''
+
+
 import requests
+import os
+
+from sendNotify import send
+from os import environ
+
+url = "https://api.hellobike.com/api?common.welfare.signAndRecommend="
+# URL+参数,参数值为空
 
 headers = {
     'Host': 'api.hellobike.com',
@@ -16,7 +31,17 @@ headers = {
     'accept-encoding': 'gzip, deflate',
     'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
 }
+# 请求体headers参数
 
 data = '{"from":"h5","systemCode":62,"platform":4,"version":"6.25.0","action":"common.welfare.signAndRecommend","token":"cfb4950b8c4141bcba6c0c2a0144f0af"}'
-response = requests.post('https://api.hellobike.com/api?common.welfare.signAndRecommend=', headers = headers, data = data)
+# 请求体Text内容，'全部内容'
+
+response = requests.post(url=url, headers = headers, data = data).json()
+# 使用post请求执行并把内容赋值给response变量
 print(response.text)
+# 打印响应参数Text全部内容
+rep = response['data']['bountyCountToday']
+print("获得奖励金:", rep)
+title = "哈啰签到通知"
+content = "获得奖励金: " + rep
+send(title,content)
