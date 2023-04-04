d24c8b014ddda111f9cfffccc6324f82d013b7a6
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2023-04-04 15:19:33 +0800

    Update ceshi.py

--- ceshi.py
+++ ceshi.py
@@ -1,18 +1,14 @@
 import requests
 import os
 import json
-
 from sendNotify import send
 from os import environ
-
 cookie = os.environ["txspcookie"]
 title = "腾讯视频"
-
 url_qd = "https://vip.video.qq.com/rpc/trpc.new_task_system.task_system.TaskSystem/CheckIn?rpc_data=%7B%7D"
 url_gk = "https://vip.video.qq.com/rpc/trpc.new_task_system.task_system.TaskSystem/ProvideAward?rpc_data=%7B%22task_id%22:1%7D"
 
 def ten_video():
-   
     headers = {
         'user-agent':'Mozilla/5.0 (Linux; Android 11; M2104K10AC Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.72 MQQBrowser/6.2 TBS/046237 Mobile Safari/537.36 QQLiveBrowser/8.7.85.27058',
         'Content-Type':'application/json',
