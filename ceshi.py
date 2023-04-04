e45b275ff0c260af20b5bbc79ae2babeac594996
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2023-04-04 16:18:49 +0800

    Update ceshi.py

--- ceshi.py
+++ ceshi.py
@@ -15,8 +15,9 @@ def main():
         'referer':'https://film.video.qq.com/x/vip-center/?entry=common&hidetitlebar=1&aid=V0%24%241%3A0%242%3A8%243%3A8.7.85.27058%244%3A3%245%3A%246%3A%247%3A%248%3A4%249%3A%2410%3A&isDarkMode=0',
         'cookie':cookie
     }
-    response_1 = requests.get(url = url_qd,headers = headers).json()
-    print(response_1.text)
+    response_1 = requests.get(url = url_qd,headers = headers)
+    res = json.loads(response_1.text)
+    print(res)
     content = "成功"
     send(title,content)
 
