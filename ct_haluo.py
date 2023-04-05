27726876fefd5cb7e4735d51fc87d2b17af0903c
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2023-04-05 18:29:06 +0800

    Update ct_haluo.py

--- ct_haluo.py
+++ ct_haluo.py
@@ -4,65 +4,55 @@ cron: 6 14 * * * ct_haluo.py
 new Env('哈啰签到');
 
 '''
-
-
 import requests
 import os
 
 from sendNotify import send
 from os import environ
 token = os.environ["haluotoken"]
-url_qd = "https://api.hellobike.com/api?common.welfare.signAndRecommend="
-url_xx = "https://api.hellobike.com/api?user.taurus.pointInfo="
-# URL+参数,参数值为空
-
-
-headers_qd = {
+url = "https://api.hellobike.com/api?common.welfare.signAndRecommend"
+url1 = "https://api.hellobike.com/api?user.taurus.pointInfo"
+headers = {
     'Host': 'api.hellobike.com',
-    'content-length': '147',
-    'accept': 'application/json, text/plain, */*',
     'origin': 'https://m.hellobike.com',
-    'sec-fetch-dest': 'empty',
-    'content-type': 'application/json',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; Redmi K20 Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/80.0.3987.99 Mobile Safari/537.36; app=easybike; version=6.25.0',
-    'requestid': '3CU1pnCBWsuRPuh',
-    'x-requested-with': 'com.jingyao.easybike',
-    'sec-fetch-site': 'same-site',
-    'sec-fetch-mode': 'cors',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 12; 22081212C Build/SKQ1.220303.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.153 Mobile Safari/537.36; app=easybike; version=6.36.0',
     'referer': 'https://m.hellobike.com/AppPlatformH5/latest/pr_index_bounty.html',
-    'accept-encoding': 'gzip, deflate',
     'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
 }
-# 请求体headers参数
 
-headers_xx = {
+data = f'{{"from":"h5","systemCode":62,"platform":4,"version":"6.36.0","action":"common.welfare.signAndRecommend","token":"{token}"}}'
+
+headers1 = {
     'Host': 'api.hellobike.com',
-    'content-length': '147',
-    'accept': 'application/json, text/plain, */*',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 12; 22081212C Build/SKQ1.220303.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.153 Mobile Safari/537.36; app=easybike; version=6.36.0',
     'origin': 'https://m.hellobike.com',
-    'sec-fetch-dest': 'empty',
-    'content-type': 'application/json',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; Redmi K20 Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/80.0.3987.99 Mobile Safari/537.36; app=easybike; version=6.25.0',
-    'requestid': '3D2OQcPj8xeIl2c',
-    'x-requested-with': 'com.jingyao.easybike',
-    'sec-fetch-site': 'same-site',
-    'sec-fetch-mode': 'cors',
-    'referer': 'https://m.hellobike.com/AppPlatformH5/latest/index.html',
-    'accept-encoding': 'gzip, deflate',
+    'referer': 'https://m.hellobike.com/',
     'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
 }
 
-data_qd = '{"from":"h5","systemCode":62,"platform":4,"version":"6.36.0","action":"common.welfare.signAndRecommend","token":"cfb4950b8c4141bcba6c0c2a0144f0af"}'
-# 请求体Text内容，'全部内容'
+data1 = f'{{"from":"h5","systemCode":62,"platform":4,"version":"6.36.0","action":"user.taurus.pointInfo","token":"{token}","pointType":1}}'
 
-data_xx = '{"from":"h5","systemCode":62,"platform":4,"version":"6.36.0","action":"user.taurus.pointInfo","token":f"{token}","pointType":1}'
+def main():
+    response = requests.post(url=url, headers = headers, data = data)
+    result = response.json()
+#调试    print(type(result), result)
+    if result['code'] == 0:
+        bountyCountToday = result['data']['bountyCountToday']
+        rez = f"签到成功，获得{bountyCountToday}奖励金"
+        print(rez)
+        response1 = requests.post(url=url1, headers = headers1, data = data1)
+        result1 = response1.json()
+#调试        print(type(result1), result1)
+        points = result1['data']['points']
+        expiring = result1['data']['expiring']
+        rez1 = f"当前奖励金:{points}, 月底将过期{expiring}"
+        print(rez1)  
+        send("哈啰",rez + '\n' + rez1)
+    elif result['code'] == 103:
+        print("token失效")
+        send("哈啰",result['msg'])
+    else:
+        print("结束")
 
-haluo_qd = requests.post(url=url_qd, headers = headers_qd, data = data_qd).json()
-haluo_xx = requests.post(url=url_xx, headers = headers_xx, data = data_xx).json()
-# 使用post请求执行并把内容赋值给response变量
-qd = haluo_qd['data']['bountyCountToday']
-xx = str(haluo_xx['data']['points'])
-print("获得: " + qd + "奖励金" + '\n' + "当前: " + xx + "奖励金")
-title = "哈啰"
-content = "获得: " + qd + "奖励金" + '\n' + "当前: " + xx + "奖励金"
-send(title,content)
+if __name__ == '__main__':
+    main()
