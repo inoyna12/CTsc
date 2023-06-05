3bf29b133e90820712174c0b2e82a9e860f6271e
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2023-06-06 01:55:26 +0800

    Update ct_ceshi.js

--- ct_ceshi.js
+++ ct_ceshi.js
@@ -2,8 +2,8 @@ const send = (i, j) =>
 new Promise((resolve) =>
 setTimeout(() => {
 console.log(`账号:${i},模拟ID:${j}`);
-resolve(‘hello‘);
-}, Math[‘round‘](Math[‘random‘]() * 10000))
+resolve('hello');
+}, Math['round'](Math['random']() * 10000))
 );
 const start = Date.now();
 for (let i = 0; i < 50; i++) for (let j = 0; j < 5; j++) send(i, j);
