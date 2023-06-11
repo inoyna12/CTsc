e155ad8e290c808a1dffa495963dbf4d85b8a253
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2023-06-12 01:45:46 +0800

    更新 ct_mt30-15.py

--- ct_mt30-15.py
+++ ct_mt30-15.py
@@ -46,6 +46,29 @@ def mtgsigs():
     data = r['data'] 
     return Cookie, mtgsig, data
 
+def Sxin(Cookie): 
+     url = f'https://promotion.waimai.meituan.com/lottery/limitcouponcomponent/info?couponReferIds={couponReferId}' 
+     headers = { 
+         "Host":"promotion.waimai.meituan.com", 
+         "Connection":"keep-alive", 
+         "Upgrade-Insecure-Requests":"1", 
+         "User-Agent":"Mozilla/5.0 (Linux; Android 12; 22081212d Build/SKQ1.220303.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.153 Mobile Safari/537.36", 
+         "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", 
+         "dnt":"1", 
+         "X-Requested-With":"mark.via", 
+         "Sec-Fetch-Site":"none", 
+         "Sec-Fetch-Mode":"navigate", 
+         "Sec-Fetch-User":"?1", 
+         "Sec-Fetch-Dest":"document", 
+         "Accept-Encoding":"gzip, deflate, br", 
+         "Accept-Language":"zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7", 
+         "Cookie":Cookie 
+     } 
+     response = requests.get(url, headers=headers) 
+     result = response.json() 
+     print(result)
+
+
 def mt30_15():
     Cookie, mtgsig, data = mtgsigs()
     headers = {
