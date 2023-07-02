f162da96970f5d11c3c6b0a8ca706530edcddc94
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2023-07-02 11:24:21 +0800

    Update ct_jhsh.py

--- ct_jhsh.py
+++ ct_jhsh.py
@@ -7,48 +7,44 @@ import requests
 import os
 import json
 import time
-#print(f"==================脚本执行- 北京时间(UTC+8)：{time.strftime('%Y/%m/%d %H:%M:%S', time.localtime())}=====================\n")
 from sendNotify import send
 from os import environ
 
-def qiandao(Phone, MEB_ID, new_Phone):
-    try:
-        msg = ""
-        url = 'https://yunbusiness.ccb.com/clp_coupon/txCtrl?txcode=A3341A040'
-        headers = {
-            'Host': 'yunbusiness.ccb.com',
-            'channel_num': '1',
-            'mid': '160',
-            'content-type': 'application/json;charset=UTF-8',
-            'accept': 'application/json,text/javascript,*/*',
-            'user-agent': 'Mozilla/5.0 (Linux; Android 12; 22081212C Build/SKQ1.220303.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.153 Mobile Safari/537.36/CloudMercWebView',
-            'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-        }
-        json = {
-            'ACT_ID': '20230628070000000001',
-            'MEB_ID': MEB_ID,
-            'USR_TEL': Phone,
-            'REGION_CODE': '320500',
-            'chnlType': '1',
-            'regionCode': '320500',
-        } 
-        response = requests.post(url=url, headers = headers, json = json)
-        result = response.json()
-        print(result)
-        if len(result['data']) > 0:
-            msg += f"{new_Phone}：签到成功！\n"
+def sign():
+    msgs = ''
+    url = 'https://yunbusiness.ccb.com/clp_coupon/txCtrl?txcode=A3341A040'
+    headers = {
+        'Host': 'yunbusiness.ccb.com',
+        'channel_num': '1',
+        'mid': '160',
+        'content-type': 'application/json;charset=UTF-8',
+        'accept': 'application/json,text/javascript,*/*',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 12; 22081212C Build/SKQ1.220303.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.153 Mobile Safari/537.36/CloudMercWebView',
+        'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+    }
+    json = {
+        'ACT_ID': '20230628070000000001',
+        'MEB_ID': MEB_ID,
+        'USR_TEL': Phone,
+        'REGION_CODE': '320500',
+        'chnlType': '1',
+        'regionCode': '320500',
+    } 
+    response = requests.post(url=url, headers = headers, json = json)
+    result = response.json()
+    print(result)
+        if result['errMsg'] == None:
+            msgs += f"{Phones}：签到成功！\n"
             if result['data']['IS_AWARD'] == 1:
                 COUP_TITLE = result['data']['COUP_TITLE']#获得奖励
                 COUP_SUB_TITLE = result['data']['COUP_SUB_TITLE']#满多少可用
-                msg += f"获得：{COUP_TITLE}，{COUP_SUB_TITLE}\n"  
+                msgs += f"获得：{COUP_TITLE}，{COUP_SUB_TITLE}\n"  
             NEST_AWARD_DAY = result['data']['NEST_AWARD_DAY']#剩余天数
             SIGN_TIPS = result['data']['SIGN_TIPS']#可获得奖励
-            msg += f"再签到{NEST_AWARD_DAY}天获得{SIGN_TIPS}\n\n" 
-        elif len(result['data']) == 0: 
-            msg += f"{new_Phone}：请勿重复签到！\n\n"
-    except:
-        msg += f"Cookie失效或脚本待更新\n"
-    return msg
+            msgs += f"再签到{NEST_AWARD_DAY}天获得{SIGN_TIPS}\n\n" 
+        else:
+            msgs += f"{Phones}：{result['errMsg']}\n\n"
+    return msgs
 
 def get_act_id(MEB_ID):
     url = 'https://yunbusiness.ccb.com/basic_service/txCtrl?txcode=A3341AB03'
@@ -69,30 +65,38 @@ def get_act_id(MEB_ID):
     print(id)
     return id
 
+def random_sleep(min_val, max_val):
+    num = random.randint(min_val, max_val)
+    print(f"等待{num}秒后继续>>>>>>>>>>>")
+    time.sleep(num)
+
 def ql_env():
-    if "jhshcookie" in os.environ:
-        token_list = os.environ['jhshcookie'].split('\n')
+    if env_name in os.environ:
+        token_list = os.environ[env_name].split('\n')
         if len(token_list) > 0:
             return token_list
         else:
-            print("jhshcookie变量未启用")
+            print("变量未启用")
             sys.exit(1)
     else:
-        print("未添加jhshcookie变量")
+        print("未添加变量")
         sys.exit(0)
 
 if __name__ == '__main__':
+    env_name = "jhshID"
+    title_name = '建行生活'
     msg = ""
-    index = 1
-    quantity = ql_env()
+    index = 0
+    quantity = ql_env(env_name)
     print (f"共找到{len(quantity)}个账号")
     for cookie in quantity:
-        print (f"------------正在执行第{index}个账号----------------")
-        Phone = cookie.split('&')[0]
-        MEB_ID = cookie.split('&')[1]
-        new_Phone = Phone[:3] + "****" + Phone[7:]
-        msg += qiandao(Phone, MEB_ID, new_Phone)
-        print(msg)
-        time.sleep(10)
+        print (f"------------正在执行第{index + 1}个账号----------------")
+        Phone = cookie.split('#')[0]
+        MEB_ID = cookie.split('#')[1]
+        Phones = Phone[:3] + "****" + Phone[7:]
+        msg += sign()
+        print(f"第{index + 1}个账号运行完成\n")
         index += 1
-    send('建行生活', msg)
+        if index < len(quantity):
+            random_sleep(10, 20)
+    send(title_name, msg)
