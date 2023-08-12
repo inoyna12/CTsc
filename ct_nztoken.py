8b9c9f7720cd99e49c3495f4a08b4da5fd94a6e9
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2023-08-13 07:40:45 +0800

    Update ct_nztoken.py

--- ct_nztoken.py
+++ ct_nztoken.py
@@ -20,6 +20,8 @@ if __name__ == '__main__':
     quantity1 = ql_env("NZmy_phone")
     quantity2 = ql_env("NZphone")
     quantity3 = ql_env("NZtoken")
+    print(len(quantity1))
+    print(len(quantity2), len(quantity3))
     if len(quantity2) != len(quantity3):
         print("变量列表数量不相等")
         exit() # 停止运行
