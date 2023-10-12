e9a860489a229a7bfc3a21d3b8a35d0389b92a07
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2023-10-13 02:50:22 +0800

    Update ct_nzqc_ceshi.py

--- ct_nzqc_ceshi.py
+++ ct_nzqc_ceshi.py
@@ -1,39 +1,21 @@
 '''
 cron: 36 0 * * *
-new Env('哪吒汽车重置json');
+new Env('nzqc重置');
 '''
 
-import os,json
-# 假设有两个列表，分别存储了账号和密码
-accounts = os.environ['NZphone'].split('\n')
-passwords = os.environ['NZtoken'].split('\n') + os.environ['NZtoken1'].split('\n')
+import json,random
 
-# 初始化空列表
-account_list = []
-
-# 遍历账号和密码列表
-for i in range(len(accounts)):
-    account = accounts[i]
-    password = passwords[i]
-    
-    # 创建包含账号、密码和其他键值对的字典
-    account_dict = {
-        "mobile": account,
-        "refresh_token": password,
-        "refresh_token_time": None,
-        "access_token": None,
-        "access_token_time": None,
-        "sign": False,
-        "share": 0,
-        "comment": 0,
-        "creditScore": 0,
-        "reserve": None,
-        "reserve2": None
-    }
-    
-    # 将每个账号和密码以及其他键值对的字典添加到列表中
-    account_list.append(account_dict)
-print(len(account_list))
 filepath = "/ql/data/env/nzqc.json"
+with open(filepath, "r") as f:
+    data = json.load(f)
+    
+for i in data:
+    i["sign"] = False
+    i['share'] = 0
+    i['comment'] = 0
+   
 with open(filepath, "w") as f:
-    json.dump(account_list, f)
+    json.dump(data, f)    
+    
+print(len(data))
+#random.shuffle(account_list)
