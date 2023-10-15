ce0751d2809474c22092630ea31a5a13df980cb6
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2023-10-16 05:19:32 +0800

    Update ct_nzqc_reset.py

--- ct_nzqc_reset.py
+++ ct_nzqc_reset.py
@@ -13,9 +13,9 @@ for i in data:
     i["sign"] = False
     i['share'] = 0
     i['comment'] = 0
-   
+    
+random.shuffle(data)  
 with open(filepath, "w") as f:
     json.dump(data, f)    
     
 print(len(data))
-#random.shuffle(account_list)
