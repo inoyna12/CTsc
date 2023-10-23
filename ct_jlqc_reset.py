e9cd3db3c5ab2c7750bcdcd2d0d7383ad9af4d65
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2023-10-24 00:06:36 +0800

    Create ct_jlqc_reset.py

---
+++ ct_jlqc_reset.py
@@ -0,0 +1,20 @@
+'''
+cron: 16 0 * * *
+new Env('jlqc重置');
+'''
+
+import json,random
+
+filepath = "/ql/data/env/jlqc.json"
+with open(filepath, "r") as f:
+    data = json.load(f)
+    
+for i in data:
+    i["sign"] = False
+    i['create'] = False
+    
+random.shuffle(data)  
+with open(filepath, "w") as f:
+    json.dump(data, f)    
+    
+print(len(data))
