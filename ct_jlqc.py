485abcc1751c2df190faa750f86d07ead73ab639
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2023-11-21 09:19:53 +0800

    Update ct_jlqc.py

--- ct_jlqc.py
+++ ct_jlqc.py
@@ -457,7 +457,7 @@ if __name__ == '__main__':
         print(f"第{index + 1}个账号运行完成")
         index += 1
         if index < len(info_new):
-            random_sleep(0, 100)
+            random_sleep(0, 50)
     info_filtered = [info for info in info_new if info["token_status"]]
     with open(filepath, 'w') as f:
         json.dump(info_filtered, f)
