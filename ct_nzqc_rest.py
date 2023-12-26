6671d2be2855dbb16378d555669c14a8a9b08f67
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2023-12-26 19:54:12 +0800

    Update ct_nzqc_rest.py

--- ct_nzqc_rest.py
+++ ct_nzqc_rest.py
@@ -3,15 +3,7 @@ cron: 16 0 * * *
 new Env('nzqc测试');
 '''
 
-import json,random,os
-from utils.ql_api import get_envs, put_envs
-
-env = 'TZC_num2'
-phone_list = os.environ[env].split('\n')
-if phone_list[0] == '空':
-    print("环境变量为空，不需要添加")
-    exit()
-    
+import json,random
 
 filepath = "/ql/data/env/nzqc.json"
 with open(filepath, "r") as f:
@@ -19,9 +11,8 @@ with open(filepath, "r") as f:
 print(len(data))
 
 for info in data:
-    info['tzc_num2'] = 0
-    if info['mobile'] in phone_list:
-        info['tzc_num2'] = 2
+    if 'reserve' in info:
+        del info['comment']
 
 with open(filepath, "w") as f:
     json.dump(data, f)
