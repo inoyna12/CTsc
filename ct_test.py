da801673f2d1d288ccd9080fce8ae0c431fdba47
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2024-02-17 06:06:34 +0800

    Update ct_test.py

--- ct_test.py
+++ ct_test.py
@@ -1,1 +1,16 @@
+'''
+cron: 0 0 * * *
+new Env('测试脚本');
+'''
+import json
+filepath = "/ql/data/env/dyxw.json"
+with open(filepath, 'r', encoding='utf-8') as file:
+    data_list = json.load(file)
 
+new_key_value_pairs = {'ref_code': None, 'taskStatus': False}
+
+for d in data_list:
+    d.update(new_key_value_pairs)
+    
+with open(filepath, 'w', encoding='utf-8') as file:
+    json.dump(data_list, file, indent=2)
