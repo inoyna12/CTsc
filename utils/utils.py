dbc6a9e722b635d7d6a92d8ed59b173d94dee6c8
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2024-02-17 02:08:37 +0800

    Create utils.py

---
+++ utils/utils.py
@@ -0,0 +1,33 @@
+import time,random,uuid,sys,hashlib
+
+# 随机延迟
+def random_sleep(min_val, max_val):
+    num = random.randint(min_val, max_val)
+    print(f"随机等待{num}秒后继续>>>>>>>>>>>")
+    time.sleep(num)
+
+# 生成uuid    
+def random_uuid():
+    random_uuid = str(uuid.uuid4())
+    return random_uuid
+    
+# 修改print方法 避免某些环境下python执行print 不会去刷新缓存区导致信息第一时间不及时输出
+def print_now(content):
+    print(content)
+    sys.stdout.flush()
+    
+# 生成时间戳
+def timestamp(length):
+    if length == 10:
+        return int(round(time()))
+    elif length == 16:
+        return int(round(time() * 1000))
+    else:
+        print("时间戳生成失败")
+        return
+
+# sha256加密        
+def sha256_encode(string):
+    hash_object = hashlib.sha256(string.encode('utf-8'))
+    hex_dig = hash_object.hexdigest()
+    return hex_dig
