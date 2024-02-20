ed806441fdf65da23dd531dc30352d0bccf05473
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2024-02-21 06:54:26 +0800

    Update ct_test.py

--- ct_test.py
+++ ct_test.py
@@ -3,14 +3,15 @@ cron: 0 0 * * *
 new Env('测试脚本');
 '''
 import json
-filepath = "/ql/data/env/dyxw.json"
+filepath = "/ql/data/env/nzqc.json"
 with open(filepath, 'r', encoding='utf-8') as file:
     data_list = json.load(file)
 
-new_key_value_pairs = {'ref_code': None, 'taskStatus': False}
+new_key_value_pairs = {'miScales2': None, 'miHairDryer': None}
 
 for d in data_list:
+    del d['tzc_num2']
     d.update(new_key_value_pairs)
     
 with open(filepath, 'w', encoding='utf-8') as file:
-    json.dump(data_list, file, indent=2)
+    json.dump(data_list, file)
