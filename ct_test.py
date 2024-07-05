04340d56b5fc8623a2381a28058989b3ed65c86f
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2024-07-06 01:39:12 +0800

    Update ct_test.py

--- ct_test.py
+++ ct_test.py
@@ -3,15 +3,14 @@ cron: 0 0 * * *
 new Env('测试脚本');
 '''
 import json
-filepath = "/ql/data/env/nzqc.json"
-with open(filepath, 'r', encoding='utf-8') as file:
-    data_list = json.load(file)
+filepath = "/ql/data/env/jlqc.json"
+with open(filepath, 'r') as f:
+    atAll_list = json.load(f)
 
-new_key_value_pairs = {'miScales2': None, 'miHairDryer': None}
-
-for d in data_list:
-    del d['tzc_num2']
-    d.update(new_key_value_pairs)
-    
-with open(filepath, 'w', encoding='utf-8') as file:
-    json.dump(data_list, file)
+for at_dict in atAll_list:
+    if at_dict['phone'] == '19941326235':
+        del atAll_list[at_dict]
+        break
+        
+with open(filepath, 'w') as f:
+    json.dump(atAll_list, f, indent=2)
