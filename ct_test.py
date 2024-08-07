a2da617e578029aea76af3e833fa995c4bbe4de3
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2024-08-08 06:36:41 +0800

    Update ct_test.py

--- ct_test.py
+++ ct_test.py
@@ -1,16 +1,36 @@
-'''
-cron: 0 0 * * *
-new Env('测试脚本');
-'''
-import json
-filepath = "/ql/data/env/jlqc.json"
-with open(filepath, 'r') as f:
-    atAll_list = json.load(f)
+import requests,time
+#import curl_cffi.requests as requests
+# 提取代理API接口，获取1个代理IP
+#api_url = "http://v2.api.juliangip.com/dynamic/getips?filter=1&num=1&pt=1&result_type=json&trade_no=1282958555546938&sign=31a2348320c6b0938bb566fdd4e37b7e"
+api_url = "http://v2.api.juliangip.com/company/postpay/getips?num=1&pt=1&result_type=json&trade_no=6130652715138961&sign=3b1896626239e61a182b00ac5582d07f"
+# 获取API接口返回的代理IP
+# proxy_ip = requests.get(api_url).json()
+# proxy_list_ip = proxy_ip['data']['proxy_list'][0]
+# print("当前代理ip：" + proxy_list_ip)
 
-for i, at_dict in enumerate(atAll_list):
-    if at_dict['phone'] == '19941326235':
-        del atAll_list[i]
-        break
-        
-with open(filepath, 'w') as f:
-    json.dump(atAll_list, f, indent=2)
+# proxies = {
+  # "http": proxy_list_ip,
+  # "https": proxy_list_ip,
+# }
+
+def get_public_ip():
+    try:
+        response = requests.get('http://ipinfo.io/json', proxies=proxies)
+        result = response.json()
+        print(result['ip'])
+    except Exception as e:
+        print(e)
+
+
+for i in range(10):
+    proxy_ip = requests.get(api_url).json()
+    print(proxy_ip)
+    proxy_list_ip = proxy_ip['data']['proxy_list'][0]
+    print("当前代理ip：" + proxy_list_ip)
+    
+    proxies = {
+      "http": proxy_list_ip,
+      "https": proxy_list_ip,
+    }
+    get_public_ip()
+    time.sleep(10)
