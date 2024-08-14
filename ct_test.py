b7755b8085305bf361625738b19daddf415d867d
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2024-08-14 15:07:56 +0800

    Update ct_test.py

--- ct_test.py
+++ ct_test.py
@@ -4,21 +4,17 @@ new Env('吉利汽车更新账号');
 '''
 import json
 import os,random
-from utils.github_file_manager import GithubFileManager
+from github import Github
+
 
 ql_filepath = "/ql/data/env/jlqc.json"
+g = Github(os.getenv('github_token'))
+repo = g.get_repo("inoyna12/updateTeam")
+file_path_in_repo = "吉利汽车/test.json"
+commit_message = 'Added a json file'
 
-access_token = os.getenv('github_token')
-repo_name = "inoyna12/updateTeam"
-branch = "master"
-gh_filepath = "吉利汽车/test.json"
-#new_content = json.dumps([])
-commit_message = f"Update {gh_filepath}"
+with open(ql_filepath, 'r') as file:
+    content = file.read()
 
 
-fileManager = GithubFileManager(access_token)
-with open(ql_filepath, 'r') as f:
-    accountAll_list = json.load(f)
-# with open(ql_filepath, 'w') as f:
-    # json.dump(accountAll_list, f, indent=2)
-fileManager.update_file_content(repo_name, gh_filepath, accountAll_list, commit_message, branch)
+repo.create_file(file_path_in_repo, commit_message, content)
