5a3d561cfdada499b81e8dee4585da19758bfe3e
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2024-08-17 08:54:59 +0800

    Update ct_jlqcUpdateToken.py

--- ct_jlqcUpdateToken.py
+++ ct_jlqcUpdateToken.py
@@ -5,13 +5,46 @@ new Env('吉利汽车刷新token');
 import requests
 import json
 from utils.utils import randomSleep,send_request
+from github import Github
 from notify import send
 
 title_name = '吉利汽车刷新token'
-version = "3.20.0"
+version = "3.23.2"
+
+success_num = 0
+fail_num = 0
+    
 filepath = "/ql/data/env/jlqc.json"
+with open(filepath, 'r') as f:
+    all_data = json.load(f)
+
+gh = Github(os.getenv('github_token'))
+gh_repo = gh.get_repo("inoyna12/updateTeam")
+gh_file_path = "吉利汽车/AccountInfo.json"
+gh_commit_message = "Updated the file"
+gh_file_info = gh_repo.get_contents(gh_file_path)
+gh_file_content = json.loads(gh_file_info.decoded_content.decode('utf-8'))
+
+def get_proxies():
+    for i in range(3):
+        proxy_json = send_request("http://v2.api.juliangip.com/company/postpay/getips?num=1&pt=1&result_type=json&trade_no=6130652715138961&sign=3b1896626239e61a182b00ac5582d07f", 'GET')
+        if proxy_json['code'] == 200:
+            proxy_ip = proxy_json['data']['proxy_list'][0]
+            print("当前代理IP：" + proxy_ip)
+            proxies = {
+              "http": proxy_ip,
+              "https": proxy_ip,
+            }
+            return proxies
+        else:
+            print(proxy_json)
+            time.sleep(60)
+    send(title_name + "---异常", "获取代理IP失败！")
+    exit()
 
 def refresh():
+    proxies = get_proxies()
+    global success_num, fail_num
     url = "https://app.geely.com/api/v1/user/refresh"
     params = {
         'refreshToken': at_dict['refreshToken']
@@ -23,29 +56,24 @@ def refresh():
         "appversion": version,
         "platform": "Android"
     }
-    result = send_request(url, 'GET', params=params, headers=headers)
+    result = send_request(url, 'GET', params=params, headers=headers, proxies=proxies)
     print(result)
     if result['code'] == "success":
         at_dict['token'] = result['data']['token']
         at_dict['refreshToken'] = result['data']['refreshToken']
-        global success_num
         success_num = success_num + 1
     else:
-        global fail_num
         fail_num = fail_num + 1
 
 if __name__ == '__main__':
-    success_num = 0
-    fail_num = 0
-    with open(filepath, 'r') as f:
-        accountAll_list = json.load(f)
-    print(f"\n共找到{len(accountAll_list)}个账号")
-    for index, at_dict in enumerate(accountAll_list, start = 1):
-        print(f"\n{'-' * 13}正在执行第{index}/{len(accountAll_list)}个账号{'-' * 13}")
+    print(f"\n共找到{len(all_data)}个账号")
+    for index, at_dict in enumerate(all_data, start = 1):
+        print(f"\n{'-' * 13}正在执行第{index}/{len(all_data)}个账号{'-' * 13}")
         print(f"{at_dict['phone']}：")
         refresh()
-        if index < len(accountAll_list):
+        if index < len(all_data):
             randomSleep(30,60)
     with open(filepath, 'w') as f:
-        json.dump(accountAll_list, f, indent=2)
-    send(f"{title_name}：{len(accountAll_list)}", f"成功数量：{success_num}，失败数量：{fail_num}")
+        json.dump(all_data, f, indent=2)
+    gh_repo.update_file(gh_file_path, gh_commit_message, json.dumps(gh_file_content, indent=2), gh_file_info.sha)
+    send(f"{title_name}：{len(all_data)}", f"成功数量：{success_num}，失败数量：{fail_num}")
