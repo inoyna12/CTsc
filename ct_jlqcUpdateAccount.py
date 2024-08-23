7fa68fd3d64919e3dbafec95a7c83a61778aba06
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2024-08-23 19:38:32 +0800

    Update ct_jlqcUpdateAccount.py

--- ct_jlqcUpdateAccount.py
+++ ct_jlqcUpdateAccount.py
@@ -2,33 +2,30 @@
 cron: 2 0 * * *
 new Env('吉利汽车更新账号');
 '''
-import json
-import os,random
-from utils.github_file_manager import GithubFileManager
+
+import json,os
+from github import Github
 from notify import send
-
-ql_filepath = "/ql/data/env/jlqc.json"
-
-access_token = os.getenv('github_token')
-repo_name = "inoyna12/updateTeam"
-branch = "master"
-gh_filepath = "吉利汽车/账号密码.json"
-new_content = json.dumps([])
-commit_message = f"Update {gh_filepath}"
-
-def get_gh_fileContent():
-    file_content = fileManager.get_file_content(repo_name, gh_filepath, branch)
-    json_file_content = json.loads(file_content)
-    if len(json_file_content) == 0:
-        print(f"{gh_filepath}  内容为空")
-        exit(0)
-    return json_file_content
-
+  
+class GithubFile:
+    def __init__(self, file_path):
+        self.gh = Github(os.getenv('github_token'))
+        self.repo = self.gh.get_repo('inoyna12/updateTeam')
+        self.file_path = file_path
+        self.commit_message = "Updated the file"
+        self.file_info = self.repo.get_contents(self.file_path)
+        self.content = json.loads(self.file_info.decoded_content.decode('utf-8'))
+        print(f"读取gtihub {self.file_path} 文件成功！")
+        
+    def update(self, new_content):
+        encoded_file_content = json.dumps(new_content, indent=2).encode('utf-8')
+        self.repo.update_file(self.file_path, self.commit_message, encoded_file_content, self.file_info.sha)
+        print(f"更新github {self.file_path} 文件成功！")
+   
 def updateFile():
     global update_num, add_num
-    gh_fileContent_list = get_gh_fileContent()
-    for gh_dict in gh_fileContent_list:
-        for at_dict in accountAll_list:
+    for gh_dict in gh_list.content:
+        for at_dict in all_data:
             if at_dict['phone'] == gh_dict['phone']:
                 at_dict['password'] = gh_dict['password']
                 at_dict['token'] = gh_dict['token']
@@ -38,21 +35,30 @@ def updateFile():
                 break
         else:
             gh_dict['availablePoint'] = 0
-            accountAll_list.append(gh_dict)
+            gh_dict['signdate'] = ''
+            all_data.append(gh_dict)
             add_num += 1
             print(f"增加次数：{add_num}，号码：{gh_dict['phone']}")
 
-if __name__ == '__main__':
-    fileManager = GithubFileManager(access_token)
-    update_num = 0
-    add_num = 0
-    with open(ql_filepath, 'r') as f:
-        accountAll_list = json.load(f)
-    updateFile()
-    random.shuffle(accountAll_list)
-    with open(ql_filepath, 'w') as f:
-        json.dump(accountAll_list, f, indent=2)
-    print('JSON数据更新完成。')
-    fileManager.update_file_content(repo_name, gh_filepath, new_content, commit_message, branch)
+
+filepath = "/ql/data/env/jlqc.json"
+with open(filepath, 'r') as f:
+    all_data = json.load(f)
     
-    send("吉利汽车", f"增加账号{add_num}次，更新账号{update_num}次")
+gh_list = GithubFile('吉利汽车/账号密码.json')   
+ 
+if len(gh_list.content) == 0:
+    print("空列表，退出运行。")
+    exit()
+    
+update_num = 0
+add_num = 0
+
+updateFile()
+
+with open(filepath, 'w') as f:
+    json.dump(all_data, f, indent=2)
+    
+gh_list.update(all_data)
+    
+send("吉利汽车", f"增加账号{add_num}次，更新账号{update_num}次")
