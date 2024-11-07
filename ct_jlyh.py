1451fd44a38570d51d828a52790835b47f4b2d79
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2024-11-08 05:11:00 +0800

    Update ct_jlyh.py

--- ct_jlyh.py
+++ ct_jlyh.py
@@ -297,7 +297,6 @@ class Jlyh:
                 'Content-Type': 'application/json; charset=utf-8',
                 'Connection': 'Keep-Alive'
             }
-            print(headers)
             result = rts('post', url, headers=headers, data=str_body, proxies=self.proxies)
             if result:
                 self.signStatus(result, my_dict)
