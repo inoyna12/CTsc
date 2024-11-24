e28a9e0d53049f4c9285c45f8ee3b5fe0f6b7832
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2024-11-24 20:50:22 +0800

    Update ct_test.py

--- ct_test.py
+++ ct_test.py
@@ -6,7 +6,7 @@ with open(ql_jlyh_filepath, 'r') as f:
     all_data = json.load(f)
     
 for i in all_data:
-    i['sharedate'] = ''
+    i['signdate'] = ''
     
 with open(ql_jlyh_filepath, 'w') as f:
      json.dump(all_data, f, indent=2)
