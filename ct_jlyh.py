bf82c0ac338ceead743f1f559a24a79b3147dd4b
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2024-11-28 09:36:59 +0800

    Update ct_jlyh.py

--- ct_jlyh.py
+++ ct_jlyh.py
@@ -654,11 +654,11 @@ class Jlyh:
                 # self.signAdd(my_dict)
             # else:
                 # print("已签到，跳过")
-            if my_dict['sharedate'] != today_date:
-                self.share(my_dict)
-            else:
-                print("已分享，跳过")
-            self.getPoints(my_dict)
+            # if my_dict['sharedate'] != today_date:
+                # self.share(my_dict)
+            # else:
+                # print("已分享，跳过")
+            # self.getPoints(my_dict)
             
         if self.error > 20:
             send(f"{title_name}_错误次数过多", '\n'.join(self.error_list))
