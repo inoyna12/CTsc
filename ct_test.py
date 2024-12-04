35d2a87d2305b7567c144207df45f7f739b0b04e
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2024-12-04 09:47:04 +0800

    Update ct_test.py

--- ct_test.py
+++ ct_test.py
@@ -1,13 +1,6 @@
-import json,datetime
+from tools.githubFile import GithubFile
 
-
-ql_jlyh_filepath = "/ql/data/env/jlyh.json"
-with open(ql_jlyh_filepath, 'r') as f:
-    all_data = json.load(f)
-
-   
-for i in all_data:
-    i['sharestatus'] = 'true'
-
-with open(ql_jlyh_filepath, 'w') as f:
-     json.dump(all_data, f, indent=2)
+gh = GithubFile('test.json')
+cre = {"phone": 1}
+gh.lst.append(cre)
+gh.update(gh.lst)
