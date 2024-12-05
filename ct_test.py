b255a0709ba1e6bd44d140eccec02d37c6eaa569
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2024-12-05 15:52:44 +0800

    Update ct_test.py

--- ct_test.py
+++ ct_test.py
@@ -1,6 +1,20 @@
 from tools.githubFile import GithubFile
+import json
 
-gh = GithubFile('test.json')
-cre = {"phone": 1}
-gh.lst.append(cre)
-gh.update(gh.lst)
+ql_path = "/ql/data/env/fy.json"
+
+create_list = []
+create_dict = {
+    'phone': '15050425338',
+    'token': 'user:token:app:1705601:evos-6ebd45ebd06523e3bc4609cd94af52ac',
+    'networkState': 'WIFI',
+    'osVersion': '12',
+    'model': '22081212C',
+    'brand': 'Xiaomi'
+}
+create_list.append(create_dict)
+with open(ql_path, 'w') as f:
+    json.dump(create_list, f, indent=2)
+    
+gh_fy = GithubFile("福域/fy.json")
+gh_fy.update(create_list)
