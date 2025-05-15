8e6b430d74ae2b859e1d8c78009dabb596c594b0
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2025-05-16 01:55:33 +0800

    Update ct_test.py

--- ct_test.py
+++ ct_test.py
@@ -4,4 +4,12 @@ filepath = "/ql/data/env/jlqc.json"
 with open(filepath, 'r') as f:
     my_list = json.load(f)
 
+print(len(my_list))
+
 del_accounts = ['18501632796','19222699057','18501720352','18772451564','19222698894','19222698125','19270773419']
+new_my_list = []
+for i in my_list:
+    if i['phone'] not in del_accounts:
+        new_my_list.append(i)
+
+print(len(new_my_list))
