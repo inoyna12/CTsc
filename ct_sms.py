936100f0908349fadaffe0be52f14c7be5a8eb27
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2025-08-30 14:59:40 +0800

    Update and rename ct_jiema.py to ct_sms.py

--- ct_sms.py
+++ ct_sms.py
@@ -4,27 +4,32 @@ from datetime import datetime
 from decimal import Decimal
 from notify import send
 
-cookie = 'PHPSESSID=g30b2dhlthv2i7lg1lih2vd9l3; sl-session=MM8bSw/nr2j43G0frZqSZw=='
+cookie = 'PHPSESSID=g30b2dhlthv2i7lg1lih2vd9l3; sl-session=C04iQorRsWhxEGQZB5XWag=='
+token = 'eD/rrPvm7dDgzG8JXTUcu792PQ/c8e08bx7J7IOldlgLA2k5w+ACe6zFz4FaU9pQKk1fm3VfaGH8i9aZ67K1U7EePTGS2ndOeis7sY4en4X02vT0xcI1qT59cIjKQIJpdAdG/pLURTlC+Ztmvg1SNJcuSxXn6tkhkYGfwKJssUU='
 
 class HaoZhu:
     def __init__(self, cookie):
         self.cookie = cookie
+        self.host = 'h5.haozhuyun.com'
+        self.use_quantity = 0
+        self.use_money = Decimal(0)
     
     def headers(self):
         headers = {
-            'Host': 'h5.haozhuyun.com',
+            'Host': self.host,
             'Cookie': self.cookie
         }
         return headers
 
     # 延长cookie时间        
     def haozhu_api(self):
-        url = 'https://h5.haozhuyun.com/api.php'
-        result = requests.get(url, headers=self.headers()).json()
+        url = f'https://{self.host}/api.php'
+        result = requests.get(url, headers=self.headers(), allow_redirects=False).json()
         if result['code'] == 1:
             return True
         else:
             print(result)
+            send('豪猪', 'cookie失效')
             return False
     
     # 查询加入的对接码
@@ -32,7 +37,7 @@ class HaoZhu:
         page = 1
         docked_list = []
         while True:
-            url = f'https://h5.haozhuyun.com/api.php?type=3&gjc=&page={page}'
+            url = f'https://{self.host}/api.php?type=3&gjc=&page={page}'
             result = requests.get(url, headers=self.headers()).json()
             if result['data'] is None:
                 break 
@@ -42,7 +47,7 @@ class HaoZhu:
 
     # 删除对接码
     def del_uid(self, uid):
-        url = f'https://h5.haozhuyun.com/api.php?type=41&open=del&uid={uid}'
+        url = f'https://{self.host}/api.php?type=41&open=del&uid={uid}'
         result = requests.get(url, headers=self.headers()).json()
         print(result['msg'])
 
@@ -51,7 +56,7 @@ class HaoZhu:
         json_data = self.get_docked()
         for item in json_data:
             if item['djzt'] == '已对接':
-                parts = item['djzt'].split('/')
+                parts = item['zxky'].split('/')
                 zx = parts[0].split(':')[1]
                 ky = parts[1].split(':')[1]
                 if int(ky) == 0:
@@ -101,7 +106,7 @@ class HaoZhu:
         for i in result_list:
             str1 = f"{i['mc']}：已对接{i['djsl']}，可用号码{i['kysl']}"
             print(str1)
-            if i['djsl'] < 3 or i['kysl'] < 100:
+            if i['djsl'] < 2 or i['kysl'] < 100:
                 list1.append(str1)
         send('豪猪', '\n'.join(list1))
     
@@ -110,25 +115,63 @@ class HaoZhu:
     def get_expenses(self):
         nowdate = datetime.now().strftime('%Y-%m-%d')
         page = 1
-        money = Decimal(0)
         while True:
-            url = f'https://h5.haozhuyun.com/api.php?type=9&page={page}&rq={nowdate}&sid=&uid=&phone='
+            url = f'https://{self.host}/api.php?type=9&page={page}&rq={nowdate}&sid=&uid=&phone='
             result = requests.get(url, headers=self.headers()).json()
             if result['data'] is None:
                 break 
-            total = result['total']
+            self.use_quantity = result['total']
             for i in result['data']:
-                money += Decimal(i['dj'])
+                self.use_money += Decimal(i['dj'])
             page += 1
-        print(f"{nowdate}消费数量{total}\n{nowdate}消费金额{money}")
+        print(f"豪猪：消费数量{self.use_quantity}\n豪猪：消费金额{self.use_money}")
               
     def main(self):
         if self.haozhu_api():
             self.get_expenses()
             self.ydj_del_uid()
             self.statistics_docked()
+
+class YeZiYun:
+    def __init__(self, token):
+        self.token = token
+        self.host = 'az.yezi56.com:90'
+        self.use_quantity = 0
+        self.use_money = Decimal(0)
+    
+    def headers(self):
+        headers = {
+            'Host': self.host,
+            'Content-Length': '0'
+        }
+        return headers    
+                
+    # 获取消费记录
+    def get_expenditure(self):
+        url = f"http://{self.host}/api/get_expenditure"
+        index = 0
+        today_date = datetime.date.today()
+        while True:
+            data = {
+                "token": self.token,
+                "index": str(index)
+            }
+            result = requests.get(url, headers=self.headers()).json()
+            if result['data'] is None:
+                break 
+            for data in result['data']:
+                data_time = datetime.date.fromtimestamp(int(data['time']))
+                if today_date == data_time:
+                    self.use_quantity += 1
+                    self.use_money += Decimal(data['money'])
+                index += 40
+        print(f"椰子云：消费数量{self.use_quantity}\n椰子云：消费金额{self.use_money}")
+            
+        def main(self):
+            self.get_expenditure()
         
 haozhu = HaoZhu(cookie)
-
 haozhu.main()
 
+yeziyun = YeZiYun(token)
+yeziyun.main()
