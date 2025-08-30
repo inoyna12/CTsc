aa79bf484d1425d559a28bd6fa27fd64cbe7e40c
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2025-08-30 18:07:27 +0800

    Update ct_sms.py

--- ct_sms.py
+++ ct_sms.py
@@ -186,6 +186,5 @@ haozhu.main()
 yeziyun = YeZiYun(token)
 yeziyun.main()
 
-print(f'总消费数量{haozhu.use_quantity + yeziyun.use_quantity}\n
-        总消费金额{Decimal(haozhu.use_money) + Decimal(yeziyun.use_money)}')
+print(f'总消费数量{haozhu.use_quantity + yeziyun.use_quantity}\n总消费金额{Decimal(haozhu.use_money) + Decimal(yeziyun.use_money)}')
 
