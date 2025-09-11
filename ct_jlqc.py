12e04f1f987af87fd574672bf3c4de289c471264
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2025-09-11 09:56:12 +0800

    Update ct_jlqc.py

--- ct_jlqc.py
+++ ct_jlqc.py
@@ -221,7 +221,7 @@ class JLQC:
             if self.day in (1, 15):
                 self.refresh_token()
                 
-        if self.sign_fail >= 20 or len(self.tokenExpired_list) >= 20:
+        if self.sign_fail >= 20 or len(self.tokenExpired_list) >= 4000:
             send(f"{title_name}_异常次数过多", "异常次数过多")
             exit()
 
