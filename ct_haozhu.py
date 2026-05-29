7cbdbd2ae176028ea339dffeab5152d2982ef850
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2026-05-29 13:52:12 +0800

    Update ct_haozhu.py

--- ct_haozhu.py
+++ ct_haozhu.py
@@ -119,6 +119,7 @@ class HaoZhu:
             if result['data'] is None:
                 break
             for item in result['data']:
+                print(item)
                 zx = int(item['zxky'].split('/')[0].split(':')[1])
                 ky = int(item['zxky'].split('/')[1].split(':')[1])
                 if item['djzt'] == '已对接' and ky > 0:
